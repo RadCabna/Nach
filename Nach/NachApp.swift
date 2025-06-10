1adcbbd137a9c7462abd39ae4bda7ec697527165
Radcabna <radcabna@gmail.com>
2025-06-10 15:12:11 +0300

    Initial Commit

---
+++ Nach/NachApp.swift
@@ -0,0 +1,17 @@
+//
+//  NachApp.swift
+//  Nach
+//
+//  Created by Алкександр Степанов on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
